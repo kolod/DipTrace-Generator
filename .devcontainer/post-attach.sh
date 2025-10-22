ba76bf453f91c88926bb0d67876a3b4ad97dd1d0
Oleksandr Kolodkin <oleksandr.kolodkin@ukr.net>
2025-10-22 20:29:34 +0300

    devcontainer removed

--- .devcontainer/post-attach.sh
+++
@@ -1,5 +0,0 @@
-# Install dependencies
-poetry install
-
-# Set poetry virtual env as default python interpreter
-echo $(cat .vscode/settings.json) {\"python.defaultInterpreterPath\":\"$(poetry env info --executable)\"} | jq -s add > .vscode/settings.json
